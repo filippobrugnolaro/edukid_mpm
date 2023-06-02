8422074db6b61c2afc17574e793545253f270ade
marta <marta.greggio@studenti.unipd.it>
2023-06-02 14:50:20 +0200

    update main.dart

--- edukid/lib/main.dart
+++ edukid/lib/main.dart
@@ -2,11 +2,8 @@ import 'package:sizer/sizer.dart';
 
 import 'features/trivia/presentation/config/app_router.dart';
 import 'package:flutter/material.dart';
-<<<<<<< Updated upstream
 import 'package:edukid/DependencyInjectionContainer.dart' as di;
-=======
 import 'features/trivia/presentation/config/themes.dart';
->>>>>>> Stashed changes
 
 Future<void> main() async {
   await di.init();
